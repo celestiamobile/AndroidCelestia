9d237bf4630c4fa84f141f3204e4d7aae68df09b
Li Linfeng <lilinfeng303@outlook.com>
2020-03-17 21:02:57 +0800

    add action menu button

--- app/src/main/java/space/celestia/MobileCelestia/MainActivity.kt
+++ app/src/main/java/space/celestia/MobileCelestia/MainActivity.kt
@@ -2,10 +2,8 @@ package space.celestia.MobileCelestia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
 import android.util.Log
 import android.view.WindowManager
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
