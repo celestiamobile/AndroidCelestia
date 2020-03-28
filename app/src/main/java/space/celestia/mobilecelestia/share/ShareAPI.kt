9873fa1c7d4d25128bf9ce8f09f954745c85e020
Li Linfeng <lilinfeng303@outlook.com>
2020-03-28 13:42:20 +0800

    Fix wrong base url

--- app/src/main/java/space/celestia/mobilecelestia/share/ShareAPI.kt
+++ app/src/main/java/space/celestia/mobilecelestia/share/ShareAPI.kt
@@ -34,7 +34,7 @@ class ResultMap<T>(private val cls: Class<T>?) : Function<BaseResult, T?> {
 
 object ShareAPI {
     val shared: Retrofit = Retrofit.Builder()
-        .baseUrl("https://astroweather.cn/celestia/create/")
+        .baseUrl("https://astroweather.cn/celestia/")
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .addConverterFactory(GsonConverterFactory.create())
         .build()
