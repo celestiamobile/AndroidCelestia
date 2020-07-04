ce38481f4f21c5ee491a4ac8cd29ab7d39e84a1d
Levin Li <lilinfeng303@outlook.com>
2020-07-04 11:11:08 +0800

    Update copyright

--- app/src/main/java/space/celestia/mobilecelestia/favorite/FavoriteItemRecyclerViewAdapter.kt
+++ app/src/main/java/space/celestia/mobilecelestia/favorite/FavoriteItemRecyclerViewAdapter.kt
@@ -1,7 +1,7 @@
 /*
  * FavoriteItemRecyclerViewAdapter.kt
  *
- * Copyright (C) 2001-2020, the Celestia Development Team
+ * Copyright (C) 2001-2020, Celestia Development Team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
