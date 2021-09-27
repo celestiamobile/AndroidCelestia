4040bbb8568ef98a16706010cc815108f44d972d
Levin Li <lilinfeng303@outlook.com>
2021-09-27 12:23:29 +0800

    Update data version and bump app build

--- app/src/main/java/space/celestia/mobilecelestia/MainActivity.kt
+++ app/src/main/java/space/celestia/mobilecelestia/MainActivity.kt
@@ -1770,7 +1770,8 @@ class MainActivity : AppCompatActivity(R.layout.activity_main),
     }
 
     companion object {
-        private const val CURRENT_DATA_VERSION = "24"
+        private const val CURRENT_DATA_VERSION = "25"
+        // 25: 1.4.3 Localizaton update, data update
         // 24: 1.4.2 Localization update
         // 23: 1.3.3 Localization update
         // 22: 1.3.0 Localization update
