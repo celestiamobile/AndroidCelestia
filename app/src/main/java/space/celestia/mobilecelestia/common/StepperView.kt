ceb3142ff352fc9165a3b9f893cc70a59a2cc8be
Levin Li <linfel@microsoft.com>
2023-01-04 16:43:34 +0800

    Fix ref cycle on touch listener

--- app/src/main/java/space/celestia/mobilecelestia/common/StepperView.kt
+++ app/src/main/java/space/celestia/mobilecelestia/common/StepperView.kt
@@ -20,28 +20,35 @@ import android.view.View
 import android.view.View.OnTouchListener
 import android.widget.LinearLayout
 import space.celestia.mobilecelestia.R
+import java.lang.ref.WeakReference
 
-class StepperView(context: Context, attrs: AttributeSet): LinearLayout(context, attrs) {
+class StepperView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): LinearLayout(context, attrs, defStyleAttr) {
     private val startView by lazy { findViewById<View>(R.id.stepper_start) }
     private val endView by lazy { findViewById<View>(R.id.stepper_end) }
 
     var listener: Listener? = null
 
-    @SuppressLint("ClickableViewAccessibility")
-    private val subViewListener: OnTouchListener = OnTouchListener { view, event ->
-        when (event.actionMasked) {
-            MotionEvent.ACTION_DOWN -> {
-                listener?.stepperTouchDown(this, view == startView)
-            }
-            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                listener?.stepperTouchUp(this, view == startView)
-            }
-        }
-        view.onTouchEvent(event)
-    }
-
     init {
         View.inflate(context, R.layout.stepper_view, this)
+
+        val weakSelf = WeakReference(this)
+        @SuppressLint("ClickableViewAccessibility")
+        val subViewListener = OnTouchListener { view, event ->
+            val self = weakSelf.get() ?: return@OnTouchListener view.onTouchEvent(event)
+            when (event.actionMasked) {
+                MotionEvent.ACTION_DOWN -> {
+                    self.listener?.stepperTouchDown(self, view == self.startView)
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    self.listener?.stepperTouchUp(self, view == self.startView)
+                }
+            }
+            view.onTouchEvent(event)
+        }
 
         val isRTL = resources.configuration.layoutDirection == LayoutDirection.RTL
         startView.setBackgroundResource(if (isRTL) R.drawable.stepper_right else R.drawable.stepper_left)
