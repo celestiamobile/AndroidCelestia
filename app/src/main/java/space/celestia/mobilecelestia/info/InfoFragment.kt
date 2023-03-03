42ca16e3699826affdab1d423463275b406e01c2
Levin Li <lilinfeng303@outlook.com>
2023-03-03 19:51:37 +0800

    Move InfoFragment to compose

--- app/src/main/java/space/celestia/mobilecelestia/info/InfoFragment.kt
+++ app/src/main/java/space/celestia/mobilecelestia/info/InfoFragment.kt
@@ -12,34 +12,33 @@
 package space.celestia.mobilecelestia.info
 
 import android.content.Context
-import android.graphics.BitmapFactory
-import android.graphics.Rect
 import android.os.Bundle
 import android.util.LayoutDirection
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.view.isVisible
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.themeadapter.material3.Mdc3Theme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import okhttp3.OkHttpClient
-import okhttp3.Request
 import space.celestia.celestia.AppCore
 import space.celestia.celestia.Selection
 import space.celestia.mobilecelestia.R
 import space.celestia.mobilecelestia.common.NavigationFragment
+import space.celestia.mobilecelestia.compose.LinkPreview
 import space.celestia.mobilecelestia.info.model.*
 import space.celestia.mobilecelestia.utils.getOverviewForSelection
-import space.celestia.ui.linkpreview.LPLinkView
-import space.celestia.ui.linkpreview.LPLinkViewData
-import space.celestia.ui.linkpreview.LPMetadataProvider
-import java.lang.ref.WeakReference
-import java.net.MalformedURLException
 import java.net.URL
 import javax.inject.Inject
 
@@ -48,17 +47,12 @@ class InfoFragment : NavigationFragment.SubFragment() {
     private var listener: Listener? = null
     private lateinit var selection: Selection
     private var embeddedInNavigation = false
-    private var linkData: LPLinkViewData? = null
-
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var titleLabel: TextView
-    private lateinit var contentLabel: TextView
-    private lateinit var linkView: LPLinkView
 
     @Inject
     lateinit var appCore: AppCore
 
     private lateinit var objectName: String
+    private lateinit var overview: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,25 +63,93 @@ class InfoFragment : NavigationFragment.SubFragment() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(R.layout.fragment_info_list, container, false)
-
-        recyclerView = view.findViewById(R.id.list)
-        titleLabel = view.findViewById(R.id.title)
-        contentLabel = view.findViewById(R.id.content)
-        linkView = view.findViewById(R.id.link_preview)
         objectName = appCore.simulation.universe.getNameForSelection(selection)
-        if (embeddedInNavigation)
-            titleLabel.isVisible = false
-        else
-            titleLabel.text = objectName
-        contentLabel.text = appCore.getOverviewForSelection(selection)
+        overview = appCore.getOverviewForSelection(selection)
+        return ComposeView(requireContext()).apply {
+            // Dispose of the Composition when the view's LifecycleOwner
+            // is destroyed
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                Mdc3Theme {
+                    Scaffold(topBar = {}) { paddingValues ->
+                        MainScreen(systemPadding = paddingValues)
+                    }
+                }
+            }
+        }
+    }
 
-        reload(true)
-        return view
+    @Composable
+    private fun MainScreen(systemPadding: PaddingValues) {
+        var isWebInfoLoaded by remember {
+            mutableStateOf(false)
+        }
+
+        val rowModifier = Modifier.fillMaxWidth()
+
+        val isRTL = resources.configuration.layoutDirection == LayoutDirection.RTL
+        val direction = if (isRTL) androidx.compose.ui.unit.LayoutDirection.Rtl else androidx.compose.ui.unit.LayoutDirection.Ltr
+        val contentPadding = PaddingValues(
+            start = dimensionResource(id = R.dimen.common_page_medium_margin_horizontal) + systemPadding.calculateStartPadding(direction),
+            top = dimensionResource(id = R.dimen.common_page_medium_margin_vertical) + systemPadding.calculateTopPadding(),
+            end = dimensionResource(id = R.dimen.common_page_medium_margin_horizontal) + systemPadding.calculateEndPadding(direction),
+            bottom = dimensionResource(id = R.dimen.common_page_medium_margin_vertical) + systemPadding.calculateBottomPadding(),
+        )
+
+        LazyVerticalGrid(modifier = Modifier, contentPadding = contentPadding, columns = GridCells.Fixed(2), content = {
+            val hasAltSurface = (selection.body?.alternateSurfaceNames?.size ?: 0) > 0
+            val hasWebInfo = !selection.webInfoURL.isNullOrEmpty()
+
+            item(span = { GridItemSpan(2) }) {
+                Column {
+                    if (!embeddedInNavigation) {
+                        Text(text = objectName, color = MaterialTheme.colorScheme.onBackground, style = MaterialTheme.typography.headlineSmall, modifier = rowModifier.padding(bottom = dimensionResource(
+                            id = R.dimen.common_page_medium_gap_vertical
+                        )))
+                    }
+                    Text(text = overview, color = colorResource(id = R.color.material_on_background_emphasis_medium), style = MaterialTheme.typography.bodyLarge, modifier = rowModifier.padding(bottom = dimensionResource(
+                        id = R.dimen.common_page_medium_gap_vertical
+                    )))
+                    val url = selection.webInfoURL
+                    if (hasWebInfo && url != null) {
+                        LinkPreview(url = URL(url), modifier = rowModifier.padding(bottom = dimensionResource(id = R.dimen.common_page_medium_gap_vertical)), loadResult = { loadResult ->
+                            isWebInfoLoaded = loadResult
+                        }, onClick = { finalURL ->
+                            listener?.onInfoLinkMetaDataClicked(finalURL)
+                        })
+                    }
+                }
+            }
+
+            val actions = ArrayList(InfoActionItem.infoActions)
+            if (hasWebInfo && !isWebInfoLoaded)
+                actions.add(InfoWebActionItem())
+            if (hasAltSurface)
+                actions.add(AlternateSurfacesItem())
+
+            val count = actions.size
+            items(actions.mapIndexed { index, infoActionItem -> Pair(index, infoActionItem) }) { item ->
+                val horizontalButtonSpacing = R.dimen.common_page_medium_gap_horizontal
+                val verticalButtonSpacing = R.dimen.common_page_button_gap_vertical
+                val index = item.first
+                FilledTonalButton(
+                    modifier = Modifier.padding(
+                        start = if (index % 2 == 0) 0.dp else dimensionResource(horizontalButtonSpacing) / 2,
+                        top = if (index / 2 == 0) 0.dp else dimensionResource(verticalButtonSpacing) / 2,
+                        end =  if (index % 2 == 1) 0.dp else dimensionResource(horizontalButtonSpacing) / 2,
+                        bottom = if (index / 2 == (count - 1) / 2) 0.dp else dimensionResource(verticalButtonSpacing) / 2,
+                    ), onClick = {
+                    listener?.onInfoActionSelected(item.second, selection)
+                }) {
+                    Text(text = item.second.title)
+                }
+            }
+        })
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -106,12 +168,6 @@ class InfoFragment : NavigationFragment.SubFragment() {
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-
-        linkData?.image?.recycle()
-    }
-
     override fun onDetach() {
         super.onDetach()
         listener = null
@@ -120,123 +176,6 @@ class InfoFragment : NavigationFragment.SubFragment() {
     interface Listener {
         fun onInfoActionSelected(action: InfoActionItem, item: Selection)
         fun onInfoLinkMetaDataClicked(url: URL)
-    }
-
-    private fun reload(fetchData: Boolean) {
-        val hasAltSurface = (selection.body?.alternateSurfaceNames?.size ?: 0) > 0
-        val webInfoURL = selection.webInfoURL
-        var hasWebInfo = false
-        var hideWebInfo = false
-        val linkData = this.linkData
-        if (linkData != null) {
-            hasWebInfo = true
-            hideWebInfo = true
-            linkView.linkData = linkData
-            linkView.visibility = View.VISIBLE
-            val weakSelf = WeakReference(this)
-            linkView.setOnClickListener {
-                weakSelf.get()?.listener?.onInfoLinkMetaDataClicked(linkData.url)
-            }
-        } else if (webInfoURL != null) {
-            linkView.linkData = null
-            linkView.visibility = View.GONE
-            try {
-                val url = URL(webInfoURL)
-                hasWebInfo = true
-                if (fetchData) {
-                    val weakSelf = WeakReference(this)
-                    fetchMetadata(url, textOnlyDataReadyBlock = {
-                        val self = weakSelf.get() ?: return@fetchMetadata
-                        val oldData = self.linkData
-                        self.linkData = it
-                        self.reload(false)
-                        oldData?.image?.recycle()
-                    }, imageDataReadyBlock = {
-                        val self = weakSelf.get() ?: return@fetchMetadata
-                        val oldData = self.linkData
-                        self.linkData = it
-                        self.reload(false)
-                        oldData?.image?.recycle()
-                    })
-                }
-            }
-            catch(ignored: MalformedURLException) {}
-        } else {
-            linkView.linkData = null
-            linkView.visibility = View.GONE
-        }
-        val manager = GridLayoutManager(context, 2)
-        manager.spanSizeLookup = SizeLookup()
-        recyclerView.layoutManager = manager
-        val actions = ArrayList<InfoItem>()
-        val otherActions = ArrayList(InfoActionItem.infoActions)
-        if (hasWebInfo && !hideWebInfo)
-            otherActions.add(InfoWebActionItem())
-        if (hasAltSurface)
-            otherActions.add(AlternateSurfacesItem())
-        otherActions.add(SubsystemActionItem())
-        otherActions.add(MarkItem())
-        actions.addAll(otherActions)
-        recyclerView.adapter = InfoRecyclerViewAdapter(actions, selection, listener)
-        while (recyclerView.itemDecorationCount > 0) {
-            recyclerView.removeItemDecorationAt(0)
-        }
-        recyclerView.addItemDecoration(SpaceItemDecoration())
-    }
-
-    @Suppress("BlockingMethodInNonBlockingContext")
-    private fun fetchMetadata(url: URL, textOnlyDataReadyBlock: (LPLinkViewData) -> Unit, imageDataReadyBlock: (LPLinkViewData) -> Unit) {
-        val fetcher = LPMetadataProvider()
-        fetcher.startFetchMetadataForURL(lifecycleScope, url) { metaData, _ ->
-            if (metaData == null) { return@startFetchMetadataForURL }
-            textOnlyDataReadyBlock(LPLinkViewData(metaData.url, metaData.title, null, true))
-            val imageURL = metaData.imageURL ?: metaData.iconURL
-            val usesIcon = metaData.imageURL == null
-            val client = OkHttpClient()
-            val image = withContext(Dispatchers.IO) {
-                try {
-                    val req = Request.Builder().url(imageURL).build()
-                    val res = client.newCall(req).execute()
-                    val stream = res.body?.byteStream() ?: return@withContext null
-                    return@withContext BitmapFactory.decodeStream(stream)
-                } catch(ignored: Throwable) {
-                    return@withContext null
-                }
-            }
-            if (image != null) {
-                imageDataReadyBlock(LPLinkViewData(metaData.url, metaData.title, image, usesIcon))
-            }
-        }
-    }
-
-    inner class SizeLookup: GridLayoutManager.SpanSizeLookup() {
-        override fun getSpanSize(position: Int): Int {
-            return 1
-        }
-    }
-
-    inner class SpaceItemDecoration : RecyclerView.ItemDecoration() {
-        override fun getItemOffsets(
-            outRect: Rect,
-            view: View,
-            parent: RecyclerView,
-            state: RecyclerView.State
-        ) {
-            val isRTL = parent.resources.configuration.layoutDirection == LayoutDirection.RTL
-            val horizontalButtonSpacing = resources.getDimensionPixelOffset(R.dimen.common_page_medium_gap_horizontal)
-            val verticalButtonSpacing = resources.getDimensionPixelOffset(R.dimen.common_page_button_gap_vertical)
-
-            val pos = parent.getChildLayoutPosition(view)
-            outRect.top = 0
-            outRect.bottom = verticalButtonSpacing
-            if (pos % 2 == 1) {
-                outRect.left = if (isRTL) 0 else (horizontalButtonSpacing / 2)
-                outRect.right = if (isRTL) (horizontalButtonSpacing / 2) else 0
-            } else {
-                outRect.left = if (isRTL) (horizontalButtonSpacing / 2) else 0
-                outRect.right = if (isRTL) 0 else (horizontalButtonSpacing / 2)
-            }
-        }
     }
 
     companion object {
