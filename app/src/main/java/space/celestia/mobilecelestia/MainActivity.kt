96ae53655ab7ae0dde4ab01fab38aa6f50836500
Levin Li <lilinfeng303@outlook.com>
2023-08-10 00:24:37 +0800

    Bump data version

--- app/src/main/java/space/celestia/mobilecelestia/MainActivity.kt
+++ app/src/main/java/space/celestia/mobilecelestia/MainActivity.kt
@@ -2168,7 +2168,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main),
 
     companion object {
         private const val CURRENT_DATA_VERSION = "51"
-        // 51: 1.5.25 Dev Localization, data update (commit 604e02d462a4db4b31a43f0c98e04f57d996c6d5)
+        // 52: 1.5.26 Localization
         // 50: 1.5.24 Shader, localization, data update (commit a269ed58c91135991eba927bfe6c2dd60e774676)
         // 45: 1.5.23 Localization update, data update (commit 809c3fa60fb52667c4fc073328654549b253f493)
         // 44: 1.5.20 Localization update
