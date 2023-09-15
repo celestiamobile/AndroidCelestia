67bbccf6bb61f76a9de0f9571202f794062aa6ad
Levin Li <lilinfeng303@outlook.com>
2023-09-15 16:54:17 +0800

    Show search in toolbar

--- app/src/main/java/space/celestia/mobilecelestia/celestia/CelestiaControlView.kt
+++ app/src/main/java/space/celestia/mobilecelestia/celestia/CelestiaControlView.kt
@@ -21,7 +21,7 @@ import space.celestia.mobilecelestia.R
 import space.celestia.mobilecelestia.common.StandardImageButton
 
 enum class CelestiaControlAction {
-    ShowMenu, ToggleModeToCamera, ToggleModeToObject, Info, Hide, Show
+    ShowMenu, ToggleModeToCamera, ToggleModeToObject, Info, Search, Hide, Show
 }
 
 interface CelestiaControlButton
