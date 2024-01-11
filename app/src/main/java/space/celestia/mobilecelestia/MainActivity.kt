67a3925a157b9119ea6efa6d3b8352cbc79c7aea
Levin Li <lilinfeng303@outlook.com>
2024-01-11 20:26:53 +0800

    Bump build

--- app/src/main/java/space/celestia/mobilecelestia/MainActivity.kt
+++ app/src/main/java/space/celestia/mobilecelestia/MainActivity.kt
@@ -1962,8 +1962,9 @@ class MainActivity : AppCompatActivity(R.layout.activity_main),
     }
 
     companion object {
-        private const val CURRENT_DATA_VERSION = "67"
-        // 67: 1.6.4 Dev, Localization update, data update (commit 06887c4ddf0c953cc493eb834cf253b1ba6d790b)
+        private const val CURRENT_DATA_VERSION = "68"
+        // 68: 1.6.5, Data update (commit e4e39e55caf957ac6940966947b5da7d625453e7)
+        // 67: 1.6.4, Localization update, data update (commit 06887c4ddf0c953cc493eb834cf253b1ba6d790b)
         // 63: 1.6.3, Localization update, data update (commit 321e0da37081a7e615f0698d534056f75b638f96)
         // 60: 1.6.1, Data update (commit 3a9eeb95cff1de78374643cab92fb9fe7db4f2c4)
         // 58: 1.6.0, Localization update
