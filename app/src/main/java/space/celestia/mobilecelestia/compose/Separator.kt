127a842978ff3210cd3f3b35cc7444afb4b3ce5d
Levin Li <lilinfeng303@outlook.com>
2024-02-10 00:02:15 +0800

    Fix separator

--- app/src/main/java/space/celestia/mobilecelestia/compose/Separator.kt
+++ app/src/main/java/space/celestia/mobilecelestia/compose/Separator.kt
@@ -3,7 +3,7 @@ package space.celestia.mobilecelestia.compose
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.VerticalDivider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,6 +15,6 @@ import space.celestia.mobilecelestia.R
 fun Separator(modifier: Modifier = Modifier, separatorStart: Dp = dimensionResource(id = R.dimen.partial_separator_inset_start)) {
     Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
         Spacer(modifier = Modifier.width(separatorStart))
-        VerticalDivider(thickness = dimensionResource(id = R.dimen.default_separator_height))
+        HorizontalDivider(thickness = dimensionResource(id = R.dimen.default_separator_height))
     }
 }
