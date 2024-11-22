59cad59534b194ce910ea4e2c20732f7baeb3b68
Levin Li <lilinfeng303@outlook.com>
2024-11-22 11:01:10 -0800

    Stop using symlink for Kotlin files (#99)

--- app/src/sideload/java/space/celestia/mobilecelestia/Flavor.kt
+++ app/src/sideload/java/space/celestia/mobilecelestia/Flavor.kt
@@ -1,1 +1,31 @@
-../../../../../play/java/space/celestia/mobilecelestia/Flavor.kt
+/*
+ * Flavor.kt
+ *
+ * Copyright (C) 2024-present, Celestia Development Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
+package space.celestia.mobilecelestia
+
+import io.sentry.android.core.SentryAndroid
+
+fun CelestiaApplication.setUpFlavor() {
+    SentryAndroid.init(this) { options ->
+        options.dsn = "SENTRY-DSN"
+        options.isDebug = BuildConfig.DEBUG
+        options.enableTracing = false
+        options.isAttachScreenshot = false
+        options.isAttachViewHierarchy = false
+        options.addBundleId("BUNDLE_UUID")
+        options.addBundleId("FLAVOR_BUNDLE_UUID")
+        options.addBundleId("CELESTIA_BUNDLE_UUID")
+        options.addBundleId("CELESTIA_FOUNDATION_BUNDLE_UUID")
+        options.addBundleId("LINK_PREVIEW_BUNDLE_UUID")
+        options.addBundleId("ZIP_UTILS_BUNDLE_UUID")
+        options.proguardUuid = "PROGUARD_UUID"
+    }
+}
