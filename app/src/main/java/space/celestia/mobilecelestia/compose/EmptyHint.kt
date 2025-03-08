7b2d7af69aac203d9340495261dceb8c2da2b828
Levin Li <lilinfeng303@outlook.com>
2025-03-07 20:09:36 -0800

    Add padding to empty hints

--- app/src/main/java/space/celestia/mobilecelestia/compose/EmptyHint.kt
+++ app/src/main/java/space/celestia/mobilecelestia/compose/EmptyHint.kt
@@ -2,6 +2,7 @@ package space.celestia.mobilecelestia.compose
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,7 +15,7 @@ import space.celestia.mobilecelestia.R
 
 @Composable
 fun EmptyHint(text: String, modifier: Modifier = Modifier, actionText: String? = null, actionHandler: (() -> Unit)? = null) {
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.common_page_medium_gap_vertical))) {
+    Column(modifier = modifier.padding(horizontal = dimensionResource(R.dimen.common_page_medium_margin_horizontal), vertical = dimensionResource(R.dimen.common_page_medium_margin_vertical)), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.common_page_medium_gap_vertical))) {
         Text(text = text, textAlign = TextAlign.Center, color = MaterialTheme.colorScheme.onBackground, style = MaterialTheme.typography.bodyLarge)
         if (actionText != null) {
             FilledTonalButton(onClick = {
