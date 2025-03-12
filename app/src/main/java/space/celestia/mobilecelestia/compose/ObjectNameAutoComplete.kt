cbdd66a1b166b0088996ed90f97aa6d15b7f54df
Levin Li <lilinfeng303@outlook.com>
2025-03-12 16:55:37 -0700

    Migrate AutoComplete view

--- app/src/main/java/space/celestia/mobilecelestia/compose/ObjectNameAutoComplete.kt
+++ app/src/main/java/space/celestia/mobilecelestia/compose/ObjectNameAutoComplete.kt
@@ -1,27 +1,31 @@
 package space.celestia.mobilecelestia.compose
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.view.LayoutInflater
-import android.widget.ArrayAdapter
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.widget.addTextChangedListener
-import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import space.celestia.celestia.AppCore
 import space.celestia.celestia.Completion
 import space.celestia.celestia.Selection
-import space.celestia.mobilecelestia.R
 import space.celestia.mobilecelestia.common.CelestiaExecutor
-import java.lang.ref.WeakReference
 
-@SuppressLint("InflateParams")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ObjectNameAutoComplete(executor: CelestiaExecutor, core: AppCore, name: String, modifier: Modifier = Modifier, selection: Selection, inputUpdated: (String) -> Unit, selectionUpdated: (Selection) -> Unit) {
     val coroutineScope = rememberCoroutineScope()
@@ -35,32 +39,12 @@ fun ObjectNameAutoComplete(executor: CelestiaExecutor, core: AppCore, name: Stri
         mutableStateOf(selection)
     }
 
-    class StringArrayAdapter(context: Context, var content: List<Completion> = listOf()): ArrayAdapter<String>(context, android.R.layout.simple_dropdown_item_1line) {
-        fun updateContent(content: List<Completion>) {
-            this.content = content
-        }
-
-        override fun getCount(): Int {
-            return content.size
-        }
-
-        override fun getItem(position: Int): String {
-            return content[position].name
-        }
-    }
-
-    AndroidView(factory = { context ->
-        val view = LayoutInflater.from(context).inflate(R.layout.common_auto_complete_text_view, null, false)
-        val autoCompleteTextView = view.findViewById<MaterialAutoCompleteTextView>(R.id.text_view)
-        autoCompleteTextView.setText(savedName)
-        val adapter = StringArrayAdapter(context, content = savedCompletions)
-        autoCompleteTextView.setAdapter(adapter)
-        val weakView = WeakReference(autoCompleteTextView)
-        val autoCompleteAction: (String) -> Unit = { text ->
+    var expanded by remember { mutableStateOf(false) }
+    ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = { expanded = !expanded }, modifier = modifier) {
+        SimpleTextField(value = savedName, readOnly = false, onValueChange = { text ->
             savedName = text
             inputUpdated(savedName)
             coroutineScope.launch {
-                val strongView = weakView.get() ?: return@launch
                 val results = if (text.isEmpty()) Pair(listOf(), Selection()) else withContext(executor.asCoroutineDispatcher()) {
                     val completions = core.simulation.completionForText(text, 10)
                     val fullMatch = core.simulation.findObject(text)
@@ -72,35 +56,26 @@ fun ObjectNameAutoComplete(executor: CelestiaExecutor, core: AppCore, name: Stri
                 selectionUpdated(fullMatch)
                 if (savedCompletions != completions) {
                     savedCompletions = completions
-                    adapter.updateContent(completions)
-                    if (savedCompletions.isEmpty())
-                        adapter.notifyDataSetInvalidated()
-                    else
-                        adapter.notifyDataSetChanged()
                 }
-                if (savedCompletions.isNotEmpty())
-                    strongView.showDropDown()
+                if (!expanded)
+                    expanded = true
+            }
+        }, trailingIcon = {
+            ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+        }, textStyle = MaterialTheme.typography.bodyLarge, modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryEditable).fillMaxWidth())
+        ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }, modifier = Modifier.exposedDropdownSize(matchTextFieldWidth = true)) {
+            for (completion in savedCompletions) {
+                DropdownMenuItem({
+                    Text(text = completion.name)
+                }, onClick = {
+                    selectionUpdated(completion.selection)
+                    savedName = completion.name
+                    inputUpdated(savedName)
+                    savedObject = completion.selection
+                    selectionUpdated(savedObject)
+                    expanded = false
+                }, contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding)
             }
         }
-        autoCompleteTextView.setOnFocusChangeListener { _, hasFocus ->
-            val strongView = weakView.get() ?: return@setOnFocusChangeListener
-            if (hasFocus) {
-                autoCompleteAction(strongView.text.toString())
-            }
-        }
-        autoCompleteTextView.addTextChangedListener {
-            val strongView = weakView.get() ?: return@addTextChangedListener
-            if (strongView.isPerformingCompletion) return@addTextChangedListener
-            val text = it?.toString() ?: ""
-            autoCompleteAction(text)
-        }
-        autoCompleteTextView.setOnItemClickListener { _, _, position, _ ->
-            if (position >= 0 && position < adapter.count) {
-                val selected = savedCompletions[position].selection
-                savedObject = selected
-                selectionUpdated(selected)
-            }
-        }
-        return@AndroidView view
-    }, modifier = modifier)
+    }
 }
--- app/src/main/java/space/celestia/mobilecelestia/compose/OptionSelect.kt
+++ app/src/main/java/space/celestia/mobilecelestia/compose/OptionSelect.kt
@@ -1,35 +1,92 @@
 package space.celestia.mobilecelestia.compose
 
-import android.annotation.SuppressLint
-import android.view.LayoutInflater
-import android.widget.ArrayAdapter
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
-import com.google.android.material.textfield.MaterialAutoCompleteTextView
-import space.celestia.mobilecelestia.R
+import androidx.compose.ui.graphics.takeOrElse
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
 
-@SuppressLint("InflateParams")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun OptionSelect(options: List<String>, selectedIndex: Int, modifier: Modifier = Modifier, selectionChange: (Int) -> Unit) {
-    AndroidView(factory = { context ->
-        val view = LayoutInflater.from(context).inflate(R.layout.common_options_select, null, false)
-        val autoCompleteTextView = view.findViewById<MaterialAutoCompleteTextView>(R.id.text_view)
-        val adapter = object: ArrayAdapter<String>(context, android.R.layout.simple_dropdown_item_1line) {
-            override fun getCount(): Int {
-                return options.size
+fun OptionSelect(options: List<String>, selectedIndex: Int, modifier: Modifier = Modifier, selectionChange: (Int) -> Unit, wrapWidth: Boolean = false) {
+    var expanded by remember { mutableStateOf(false) }
+    ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = { expanded = !expanded }, modifier = modifier) {
+        SimpleTextField(value = options[selectedIndex], readOnly = true, onValueChange = {}, trailingIcon = {
+            ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+        }, textStyle = MaterialTheme.typography.bodyLarge, modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryEditable).then(if (wrapWidth) Modifier.width(IntrinsicSize.Min) else Modifier.fillMaxWidth()))
+        ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }, modifier = Modifier.exposedDropdownSize(matchTextFieldWidth = true)) {
+            for (option in options.withIndex()) {
+                DropdownMenuItem({
+                    Text(text = option.value)
+                }, onClick = {
+                    selectionChange(option.index)
+                    expanded = false
+                }, contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding)
             }
+        }
+    }
+}
 
-            override fun getItem(position: Int): String {
-                return options[position]
-            }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun SimpleTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    colors: TextFieldColors = OutlinedTextFieldDefaults.colors(),
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    val textColor =
+        textStyle.color.takeOrElse {
+            val focused = interactionSource.collectIsFocusedAsState().value
+            if (focused) colors.focusedTextColor else colors.unfocusedTextColor
         }
-        autoCompleteTextView.setAdapter(adapter)
-        autoCompleteTextView.setText(options[selectedIndex])
-        autoCompleteTextView.setOnItemClickListener { _, _, position, _ ->
-            selectionChange(position)
-        }
-        return@AndroidView view
-    }, modifier = modifier)
+    val mergedTextStyle = textStyle.merge(TextStyle(color = textColor))
+
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        interactionSource = interactionSource,
+        enabled = true,
+        readOnly = readOnly,
+        textStyle = mergedTextStyle,
+        singleLine = false,
+        modifier = modifier
+    ) {
+        OutlinedTextFieldDefaults.DecorationBox(
+            value = value,
+            visualTransformation = VisualTransformation.None,
+            innerTextField = it,
+            singleLine = false,
+            enabled = true,
+            interactionSource = interactionSource,
+            contentPadding = OutlinedTextFieldDefaults.contentPadding(),
+            trailingIcon = trailingIcon,
+            colors = colors
+        )
+    }
 }
