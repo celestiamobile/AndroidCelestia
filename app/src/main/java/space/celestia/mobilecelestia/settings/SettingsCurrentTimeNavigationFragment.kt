f1d82c846bc49bb6b55ce726c5c70dc7a654778e
Levin Li <lilinfeng303@outlook.com>
2026-01-25 00:49:02 -0800

    Migrate SettingsCurrentTimeNavigationFragment

--- app/src/main/java/space/celestia/mobilecelestia/settings/SettingsCurrentTimeNavigationFragment.kt
+++ app/src/main/java/space/celestia/mobilecelestia/settings/SettingsCurrentTimeNavigationFragment.kt
@@ -10,11 +10,110 @@
 package space.celestia.mobilecelestia.settings
 
 import android.os.Bundle
-import space.celestia.mobilecelestia.common.NavigationFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.only
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
+import androidx.fragment.app.Fragment
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.ui.NavDisplay
+import space.celestia.mobilecelestia.R
+import space.celestia.mobilecelestia.compose.Mdc3Theme
+import space.celestia.mobilecelestia.settings.viewmodel.TimeSettingsPage
+import space.celestia.mobilecelestia.settings.viewmodel.TimeSettingsViewModel
+import space.celestia.mobilecelestia.utils.CelestiaString
 
-class SettingsCurrentTimeNavigationFragment: NavigationFragment() {
-    override fun createInitialFragment(savedInstanceState: Bundle?): SubFragment {
-        return SettingsCurrentTimeFragment.newInstance()
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimeSettingsContainer() {
+    val viewModel: TimeSettingsViewModel = hiltViewModel()
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    val backStack = viewModel.backStack
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text(CelestiaString("Current Time", ""))
+            }, navigationIcon = {
+                if (backStack.count() > 1) {
+                    IconButton(onClick = {
+                        backStack.removeLastOrNull()
+                    }) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_action_arrow_back),
+                            contentDescription = null
+                        )
+                    }
+                }
+            }, scrollBehavior = scrollBehavior, windowInsets = WindowInsets())
+        },
+        contentWindowInsets = ScaffoldDefaults.contentWindowInsets.only(WindowInsetsSides.Bottom),
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+    ) { paddingValues ->
+        NavDisplay(
+            backStack = backStack,
+            onBack = { backStack.removeLastOrNull() },
+            transitionSpec = {
+                slideInHorizontally(initialOffsetX = { it }) togetherWith
+                        slideOutHorizontally(targetOffsetX = { -it })
+            },
+            popTransitionSpec = {
+                slideInHorizontally(initialOffsetX = { -it }) togetherWith
+                        slideOutHorizontally(targetOffsetX = { it })
+            },
+            predictivePopTransitionSpec = {
+                slideInHorizontally(initialOffsetX = { -it }) togetherWith
+                        slideOutHorizontally(targetOffsetX = { it })
+            },
+            entryProvider = { route ->
+                when (route) {
+                    is TimeSettingsPage.Home -> NavEntry(route) {
+                        TimeSettings(paddingValues = paddingValues)
+                    }
+                }
+            }
+        )
+    }
+}
+
+class SettingsCurrentTimeNavigationFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            // Dispose of the Composition when the view's LifecycleOwner
+            // is destroyed
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                Mdc3Theme {
+                    TimeSettingsContainer()
+                }
+            }
+        }
     }
 
     companion object {
