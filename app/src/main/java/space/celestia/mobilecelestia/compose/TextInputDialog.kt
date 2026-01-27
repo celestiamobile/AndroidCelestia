65cac699dbe1d0ca054a960549040fecb3ffc0ae
Levin Li <lilinfeng303@outlook.com>
2026-01-27 14:26:32 -0800

    Request focus on text input dialogs

--- app/src/main/java/space/celestia/mobilecelestia/compose/TextInputDialog.kt
+++ app/src/main/java/space/celestia/mobilecelestia/compose/TextInputDialog.kt
@@ -16,14 +16,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import space.celestia.mobilecelestia.utils.CelestiaString
 
 @Composable
 fun TextInputDialog(onDismissRequest: () -> Unit, confirmHandler: () -> Unit, title: String, text: String?, placeholder: String? = null, textChange: (String) -> Unit) {
+    val focusRequester = remember { FocusRequester() }
     AlertDialog(onDismissRequest = onDismissRequest, confirmButton = {
         TextButton(onClick = confirmHandler) {
             Text(text = CelestiaString("OK", ""))
@@ -31,8 +36,12 @@ fun TextInputDialog(onDismissRequest: () -> Unit, confirmHandler: () -> Unit, ti
     }, title = {
         Text(text = title)
     }, text = {
-        TextField(value = text ?: "", placeholder = if (placeholder != null) { { Text(text = placeholder) } } else null, onValueChange = textChange)
+        TextField(value = text ?: "", placeholder = if (placeholder != null) { { Text(text = placeholder) } } else null, onValueChange = textChange, modifier = Modifier.focusRequester(focusRequester))
     })
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 }
 
 @Composable
