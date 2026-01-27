5c61a15b1e408a04e0e187712c85d94cc8a81ca4
Levin Li <lilinfeng303@outlook.com>
2026-01-26 18:15:54 -0800

    Migrate Stepper to native compose

--- app/src/main/java/space/celestia/mobilecelestia/compose/Stepper.kt
+++ app/src/main/java/space/celestia/mobilecelestia/compose/Stepper.kt
@@ -1,22 +1,104 @@
 package space.celestia.mobilecelestia.compose
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.viewinterop.AndroidView
-import space.celestia.mobilecelestia.common.StepperView
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.LayoutDirection
+import space.celestia.mobilecelestia.R
 
 @Composable
 fun Stepper(touchDown: (Boolean) -> Unit, touchUp: (Boolean) -> Unit) {
-    AndroidView(factory = { context ->
-        val stepperView = StepperView(context)
-        stepperView.listener = object: StepperView.Listener {
-            override fun stepperTouchDown(view: StepperView, left: Boolean) {
-                touchDown(left)
-            }
+    val startInteractionSource = remember { MutableInteractionSource() }
+    val endInteractionSource = remember { MutableInteractionSource() }
+    val isStartPressed = startInteractionSource.collectIsPressedAsState()
+    val isEndPressed = endInteractionSource.collectIsPressedAsState()
 
-            override fun stepperTouchUp(view: StepperView, left: Boolean) {
-                touchUp(left)
-            }
+    LaunchedEffect(isStartPressed.value) {
+        if (isStartPressed.value) {
+            touchDown(true)
+        } else {
+            touchUp(true)
         }
-        stepperView
-    })
+    }
+
+    LaunchedEffect(isEndPressed.value) {
+        if (isEndPressed.value) {
+            touchDown(false)
+        } else {
+            touchUp(false)
+        }
+    }
+
+    Row(
+        modifier = Modifier
+            .width(dimensionResource(id = R.dimen.stepper_width))
+            .height(dimensionResource(id = R.dimen.stepper_height))
+            .clip(RoundedCornerShape(dimensionResource(id = R.dimen.stepper_corner_radius)))
+            .background(MaterialTheme.colorScheme.secondaryContainer),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .clickable(
+                    interactionSource = startInteractionSource,
+                    indication = ripple(),
+                    onClick = {}
+                )
+                .padding(dimensionResource(R.dimen.stepper_button_padding)),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_remove),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSecondaryContainer
+            )
+        }
+        VerticalDivider(
+            modifier = Modifier
+                .width(dimensionResource(id = R.dimen.stepper_separator_width))
+                .padding(vertical = dimensionResource(id = R.dimen.stepper_separator_top_margin))
+                .background(MaterialTheme.colorScheme.outline)
+        )
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .clickable(
+                    interactionSource = endInteractionSource,
+                    indication = ripple(),
+                    onClick = {}
+                )
+                .padding(dimensionResource(R.dimen.stepper_button_padding)),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_add),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSecondaryContainer
+            )
+        }
+    }
 }
