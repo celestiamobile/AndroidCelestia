f1f1ecce4f2525ef979cf61dc6ba6f46ca678653
Levin Li <lilinfeng303@outlook.com>
2026-05-30 17:28:20 -0700

    Update according to upstream

--- Celestia/src/main/cpp/CelestiaEclipseFinder.cpp
+++ Celestia/src/main/cpp/CelestiaEclipseFinder.cpp
@@ -20,7 +20,7 @@ public:
 
     Status eclipseFinderProgressUpdate(double t) override
     {
-        return aborted ? AbortOperation : ContinueOperation;
+        return aborted ? Status::AbortOperation : Status::ContinueOperation;
     };
 
     ~EclipseSeacherWatcher() override
@@ -40,7 +40,7 @@ JNIEXPORT jlong JNICALL
 Java_space_celestia_celestia_EclipseFinder_c_1createWithBody(JNIEnv *env,
                                                                         jclass clazz,
                                                                         jlong ptr) {
-    return (long)new EclipseSeacherWatcher((Body *)ptr);
+    return reinterpret_cast<jlong>(new EclipseSeacherWatcher(reinterpret_cast<Body*>(ptr)));
 }
 
 extern "C"
@@ -48,14 +48,14 @@ JNIEXPORT void JNICALL
 Java_space_celestia_celestia_EclipseFinder_c_1destroy(JNIEnv *env,
                                                       jclass clazz,
                                                       jlong ptr) {
-    delete (EclipseSeacherWatcher *)ptr;
+    delete reinterpret_cast<EclipseSeacherWatcher*>(ptr);
 }
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_space_celestia_celestia_EclipseFinder_c_1abort(JNIEnv *env, jclass clazz,
                                                                jlong ptr) {
-    ((EclipseSeacherWatcher *)ptr)->abort();
+    reinterpret_cast<EclipseSeacherWatcher*>(ptr)->abort();
 }
 
 extern "C"
@@ -64,17 +64,17 @@ Java_space_celestia_celestia_EclipseFinder_c_1search(JNIEnv *env, jclass clazz,
                                                                 jlong ptr, jint kind,
                                                                 jdouble start_time_julian,
                                                                 jdouble end_time_julian) {
-    EclipseFinder *finder = ((EclipseSeacherWatcher *)ptr)->getFinder();
+    EclipseFinder *finder = reinterpret_cast<EclipseSeacherWatcher*>(ptr)->getFinder();
     std::vector<Eclipse> results;
-    finder->findEclipses(start_time_julian, end_time_julian, kind, results);
+    finder->findEclipses(start_time_julian, end_time_julian, static_cast<Eclipse::Type>(kind), results);
 
     using json = nlohmann::json;
     json j = json::array();
     for (const auto& result : results)
     {
         json eclipse;
-        eclipse["occulter"] = (jlong)result.occulter;
-        eclipse["receiver"] = (jlong)result.receiver;
+        eclipse["occulter"] = reinterpret_cast<jlong>(result.occulter);
+        eclipse["receiver"] = reinterpret_cast<jlong>(result.receiver);
         eclipse["startTime"] = result.startTime;
         eclipse["endTime"] = result.endTime;
         j.push_back(eclipse);
