29a489136a5099f160a658b3d7b1f68ebd8b9672
Levin Li <lilinfeng303@outlook.com>
2026-06-16 21:30:34 -0700

    Update content

--- app/src/main/java/space/celestia/mobilecelestia/MainActivity.kt
+++ app/src/main/java/space/celestia/mobilecelestia/MainActivity.kt
@@ -2324,8 +2324,8 @@ class MainActivity : AppCompatActivity(R.layout.activity_main),
         private const val GROUP_BROWSER_ITEM_GET_INFO = 9
         private const val GROUP_HEADER = 10
 
-        private const val CURRENT_DATA_VERSION = "156"
-        // 156: 1.9.18 Localization update data update (9bc74126d433b57f34b107758ef80c3d27459c30)
+        private const val CURRENT_DATA_VERSION = "157"
+        // 157: 1.9.18 Localization update data update (3d666210190e2e2dfc60550f7f719afc856306d3)
         // 147: 1.9.17 Localization update data update (7926c3ccd126dae60e702b3b6b499cf2d07e3565)
         // 142: 1.9.16 Localization update data update (fcad6702ae267ba04fce023ee71038df5c02caf8)
         // 138: 1.9.15 Localization update data update (67a542671ace4ed5c92e32519525716038498f11)
